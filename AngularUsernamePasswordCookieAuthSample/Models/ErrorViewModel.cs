aa0897ec30de477f6b67d6c8dc7ea6c3f1321dc2
Kiran Challa <kichalla@microsoft.com>
2018-01-19 05:27:33 -0800

    Add project files.

---
+++ AngularUsernamePasswordCookieAuthSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AngularUsernamePasswordCookieAuthSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
